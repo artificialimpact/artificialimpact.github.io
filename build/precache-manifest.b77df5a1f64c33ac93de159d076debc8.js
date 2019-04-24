ab8031fca5d5e397c0620ad341d67237a596a380
nomangul <nomangul2001@gmail.com>
2019-04-25 01:22:42 +0500

    gh-pages

---
+++ build/precache-manifest.b77df5a1f64c33ac93de159d076debc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4cb4d5c59ec0f5f64ac8f02deadc7fe",
+    "url": "/artificialimpact/index.html"
+  },
+  {
+    "revision": "191b9fb46815c757ffb0",
+    "url": "/artificialimpact/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/artificialimpact/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "191b9fb46815c757ffb0",
+    "url": "/artificialimpact/static/js/main.c5a0cf6e.chunk.js"
+  },
+  {
+    "revision": "83ad7314137e0e80aa9a",
+    "url": "/artificialimpact/static/js/runtime~main.a0ef5eb8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/artificialimpact/static/media/logo.5d5d9eef.svg"
+  }
+]);
