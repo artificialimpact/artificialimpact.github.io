4801e8e2e0646565d3e44b017bf0eaa1fdefe995
nomangul <nomangul2001@gmail.com>
2019-04-25 07:03:24 +0500

    deployed to github pages with announcements functionality

---
+++ precache-manifest.aac0127c8716fa9718286cf64a977247.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f9e1b7a44357e4db5a06e247f9e01cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "610d02364cc5e704b90e",
+    "url": "/static/css/2.6466deae.chunk.css"
+  },
+  {
+    "revision": "6f482d954651d58d8972",
+    "url": "/static/css/main.5bdeb019.chunk.css"
+  },
+  {
+    "revision": "610d02364cc5e704b90e",
+    "url": "/static/js/2.a7cfc97d.chunk.js"
+  },
+  {
+    "revision": "6f482d954651d58d8972",
+    "url": "/static/js/main.f97e2cdb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7ca0840258801c1ac617c9a8057311a",
+    "url": "/static/media/AI.b7ca0840.png"
+  }
+]);
