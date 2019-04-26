21eaf7c7a56a436738dee20f059e65561d73f5cd
nomangul <nomangul2001@gmail.com>
2019-04-26 11:58:03 +0500

    modify header styles for announcement and artificialimpact heading

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import styled from "styled-components";
 
 export const Header = styled.h1`
-  font-size: calc(40px + 2vmin);
   color: #000;
+  font-size: ${props =>
+    props.announcements ? "calc(20px + 2vmin)" : "calc(40px + 2vmin)"};
   font-family: "Open Sans", sans-serif;
   font-weight: 900;
   display: inline-flex;
   letter-spacing: -2px;
-  margin-top: 3%;
+  margin-top: ${props => (props.announcements ? "20%" : "-4%")};
+  @media (min-width: 900px) {
+    margin-top: ${props => (props.announcements ? "10%" : "-1%")};
+  }
 `;
