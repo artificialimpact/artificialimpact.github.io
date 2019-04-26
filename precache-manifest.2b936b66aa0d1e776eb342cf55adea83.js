1a4a7bddccaebd7057b6be8ac1a04391df76fd20
nomangul <nomangul2001@gmail.com>
2019-04-27 01:15:27 +0500

    gh-pages new deployment with disqus integration

---
+++ precache-manifest.2b936b66aa0d1e776eb342cf55adea83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384fa682977337fcf3173d28ffd3cdbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67743dc469412cd0d20a",
+    "url": "/static/css/2.6466deae.chunk.css"
+  },
+  {
+    "revision": "ef7c10a0759565970e50",
+    "url": "/static/css/main.5bdeb019.chunk.css"
+  },
+  {
+    "revision": "67743dc469412cd0d20a",
+    "url": "/static/js/2.4adce30b.chunk.js"
+  },
+  {
+    "revision": "ef7c10a0759565970e50",
+    "url": "/static/js/main.d1dcb67d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7ca0840258801c1ac617c9a8057311a",
+    "url": "/static/media/AI.b7ca0840.png"
+  }
+]);
