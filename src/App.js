26360010a43477bc494a193d2fe7fb5a720e0baf
nomangul <nomangul2001@gmail.com>
2019-04-27 01:07:21 +0500

    remove unusal imports

--- src/App.js
+++ src/App.js
@@ -4,12 +4,9 @@ import { Header } from "./components/Header";
 import { Container } from "./components/Container";
 import { SubHeading } from "./components/SubHeading";
 import { Span } from "./components/Span";
-import { AnnouncementsList } from "./components/AnnouncementsList";
 import { Image, ImageContainer } from "./components/Image";
 import { Button, ButtonContainer } from "./components/Button";
 import AI from "./assets/images/AI.png";
-import Swal from "sweetalert2";
-import { SECONDARY } from "./constants/colors";
 import { Disqus, DisqusContainer } from "./components/Disqus";
 import { Footer } from "./components/Footer";
 import animateScrollTo from "animated-scroll-to";
@@ -19,7 +16,6 @@ function App() {
   const scrollToAnnouncements = () => {
     animateScrollTo(document.querySelector("#announcements"), options);
   };
-
   return (
     <>
       <Container>
