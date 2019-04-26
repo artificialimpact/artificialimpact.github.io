1940cd638b146e7e604843d770fb1bda483e5f0e
nomangul <nomangul2001@gmail.com>
2019-04-27 00:58:36 +0500

    add options for scroll animations

---
+++ src/heplers/scrollOptions.js
@@ -0,0 +1,10 @@
+export const options = {
+  // duration of the scroll per 1000px, default 500
+  speed: 1000,
+
+  // minimum duration of the scroll
+  minDuration: 250,
+
+  // maximum duration of the scroll
+  maxDuration: 1500
+};
